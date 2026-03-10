103448837ee38c1536a66b5c8a9da05b990006e7
Daren Tan <daren28@gmail.com>
2026-03-10 18:25:36 +0800

    Fix: Javadoc for Rate and Subject

--- src/main/java/seedu/address/model/person/Subject.java
+++ src/main/java/seedu/address/model/person/Subject.java
@@ -4,13 +4,13 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
- * Represents a Person's name in the address book.
- * Guarantees: immutable; is valid as declared in {@link #isValidName(String)}
+ * Represents a Person's subject in the address book.
+ * Guarantees: immutable; is valid as declared in
+ * {@link #isValidSubject(String)}
  */
 public class Subject {
 
-    public static final String MESSAGE_CONSTRAINTS =
-            "Subject should only contain alphabetical characters and spaces, and it should not be blank";
+    public static final String MESSAGE_CONSTRAINTS = "Subject should only contain alphabetical characters and spaces, and it should not be blank";
 
     /*
      * The first character of the address must not be a whitespace,
@@ -37,7 +37,6 @@ public class Subject {
     public static boolean isValidSubject(String test) {
         return test.matches(VALIDATION_REGEX);
     }
-
 
     @Override
     public String toString() {
